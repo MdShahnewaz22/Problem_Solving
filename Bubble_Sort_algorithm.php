3e2cd66dd91f64112e723d1b67ffc36e5e7593a7
unknown <mdshahnewazs77@gmail.com>
2025-04-14 16:51:38 +0600

    bubble sort algorithm update

---
+++ Bubble_Sort_algorithm.php
@@ -0,0 +1,18 @@
+<?php
+$array=[8,7,9,5,6,4,1];
+$sortarray=babblesort($array);
+function babblesort($array){
+    $n=count($array);
+    for($i=0;$i<$n-1;$i++){
+        for($j=0;$j<$n-$i-1;$j++){
+            if($array[$j]>$array[$j+1]){
+                $temp=$array[$j];
+                $array[$j]=$array[$j+1];
+                $array[$j+1]=$temp;
+            }
+        }
+    }
+    return $array;
+}
+print_r($sortarray);
+?>

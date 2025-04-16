bb7999c20c535248660f49d38d0363527b812c4f
unknown <mdshahnewazs77@gmail.com>
2025-04-16 22:14:58 +0600

    ascending && descending oder problem solve

---
+++ Descending_Order.php
@@ -0,0 +1,5 @@
+<?php
+$numbers =[5,9,6,4,7,1,3];
+rsort($numbers); //Sorts in Descending order
+print_r($numbers);
+?>
---
+++ ascending_order.php
@@ -0,0 +1,8 @@
+<?php
+$numbers =[5,3,8,4,1,9,11];
+sort($numbers);//Sorts in ascending order
+print_r($numbers);
+
+
+
+?>
